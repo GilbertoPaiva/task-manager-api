46938c56b74528d642eb9be50cb304cb5aa6d393
GilbertoPaiva <gilberto.melo.paiva@gmail.com>
2026-02-18 21:13:14 -0300

    feat: add OpenAPI configuration and Swagger UI dependencies for API documentation

---
+++ src/main/java/com/gilberto/task_manager_api/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package com.gilberto.task_manager_api.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI taskManagerOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Task Manager API")
+                        .description("API de gerenciamento de tarefas")
+                        .version("v1"))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Swagger UI")
+                        .url("/swagger-ui.html"));
+    }
+}
+
